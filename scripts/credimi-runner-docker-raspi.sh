a411b30039b7f3ffaf06d68e332f68f7b9cb9a21
FilippoTrotter <filippotrotter99@gmail.com>
2026-02-20 11:34:45 +0100

    feat:add docker-runner-raspi script

---
+++ scripts/credimi-runner-docker-raspi.sh
@@ -0,0 +1,12 @@
+docker rm -f credimi-runner 2>/dev/null || true
+
+docker run --rm -it --name credimi-runner --network host --privileged \
+  -v adbkeys:/root/.android \
+  -e CREDIMI_PB_ADMIN=admin@example.org \
+  -e CREDIMI_PB_PASS=adminadmin \
+  -e CREDIMI_URL=https://credimi.io \
+  -e TEMPORAL_ADDRESS=temporal.credimi.io:7233 \
+  -e CREDIMI_RUNNER_ID=/forkbomb-bv-andrea/android-cloud-credimi-1 \
+  ghcr.io/forkbombeu/credimi-runner-phone:latest 127.0.0.1:5555  
+ 
+ 
